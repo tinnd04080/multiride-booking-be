7b24f77d1c39a5c593919087373387da02dfaf43
Đức Tín <tinnd040803@gmail.com>
2024-10-09 00:47:11 +0700

    login api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,5 +4,6 @@ import AuthController from "../controllers/auth.js";
 const authRouter = express.Router();
 
 authRouter.post("/register", AuthController.signUp);
+authRouter.post("/login", AuthController.signIn);
 
 export default authRouter;
