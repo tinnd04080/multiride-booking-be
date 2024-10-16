8916fa9e03afdfec3d14a4f80c7742ccf15df22b
Mẹo Nguyễn <lqnc5102004@gmail.com>
2024-10-16 23:05:35 +0700

    trip management

--- src/routes/busRoutes.js
+++ src/routes/busRoutes.js
@@ -1,13 +1,28 @@
 import express from "express";
-import { checkLogin } from "../middlewares/auth.js";
+import { checkLogin, isAdmin } from "../middlewares/auth.js";
 import BusRouteController from "../controllers/busRoutes.js";
 
 const busTypeRouter = express.Router();
 
-busTypeRouter.post("/", checkLogin, BusRouteController.createBusRoutes);
+busTypeRouter.post(
+  "/",
+  checkLogin,
+  isAdmin,
+  BusRouteController.createBusRoutes
+);
 busTypeRouter.get("/", checkLogin, BusRouteController.getBusRoutes);
 busTypeRouter.get("/:id", checkLogin, BusRouteController.getBusRoute);
-busTypeRouter.put("/:id", checkLogin, BusRouteController.updateBusRoute);
-busTypeRouter.delete("/:id", checkLogin, BusRouteController.removeBusRoute);
+busTypeRouter.put(
+  "/:id",
+  checkLogin,
+  isAdmin,
+  BusRouteController.updateBusRoute
+);
+busTypeRouter.delete(
+  "/:id",
+  checkLogin,
+  isAdmin,
+  BusRouteController.removeBusRoute
+);
 
 export default busTypeRouter;
--- src/routes/busTypes.js
+++ src/routes/busTypes.js
@@ -1,13 +1,23 @@
 import express from "express";
-import { checkLogin } from "../middlewares/auth.js";
+import { checkLogin, isAdmin } from "../middlewares/auth.js";
 import BusTypesController from "../controllers/busTypes.js";
 
 const busTypeRouter = express.Router();
 
-busTypeRouter.post("/", checkLogin, BusTypesController.createBusTypes);
+busTypeRouter.post("/", checkLogin, isAdmin, BusTypesController.createBusTypes);
 busTypeRouter.get("/", checkLogin, BusTypesController.getBusTypes);
 busTypeRouter.get("/:id", checkLogin, BusTypesController.getBusType);
-busTypeRouter.put("/:id", checkLogin, BusTypesController.updateBusType);
-busTypeRouter.delete("/:id", checkLogin, BusTypesController.removeBusType);
+busTypeRouter.put(
+  "/:id",
+  checkLogin,
+  isAdmin,
+  BusTypesController.updateBusType
+);
+busTypeRouter.delete(
+  "/:id",
+  checkLogin,
+  isAdmin,
+  BusTypesController.removeBusType
+);
 
 export default busTypeRouter;
